3a8a32b43703f3c5e846f1239f8c79c94693a964
darioo <sb.kowalwawa+github@gmail.com>
2014-06-18 00:01:25 +0200

    Update init.rb

--- init.rb
+++ init.rb
@@ -7,5 +7,7 @@ Redmine::Plugin.register :redmine_author_notification_emails do
   name 'Redmine Author Notification Emails'
   author 'Dariusz Kowalski'
   description 'This plugin change FROM adres at all Emails to Logged User email'
-  version '0.1.0' 
+  version '1.0.0' 
+  author_url 'https://github.com/darioo'
+  url "https://github.com/darioo/redmine_author_notification_emails" if respond_to?(:url)
 end
